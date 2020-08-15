a61bd7a352a6951c40c331d6228487eb799a794d
Patrick Kan <patrickkan24@gmail.com>
2020-08-15 04:08:10 -0700

    Add typed.js typing

--- public/typing.js
+++ public/typing.js
@@ -1,32 +1,80 @@
-// const typed
+var options = {
+    strings: ['choose an action...'],
+    smartBackspace: true, // Default value
+    typeSpeed: 50,
+}
 
-const actionTyping = new TypeIt("#action-text-id", {
-    strings: "choose an action...",
-    waitUntilVisible: true,
-    lifeLike: true
-}).go()
+var typed = new Typed('#action-text-id', options)
 
-const aboutTyping = new TypeIt("#action-text-id", {
-    strings: "learn more about me!",
-    waitUntilVisible: true,
-    lifeLike: true
+var aboutSelected = false;
+var resumeSelected = false;
+var githubSelected = false;
+var testSelected = false;
+
+
+// Starts with no indexs selected
+// var selectedIndex = -1;
+
+// about, resume, github, test
+// const selectedButton = [false, false, false, false]
+
+document.querySelector("#about").addEventListener('mouseenter', (e) => {
+    if(!aboutSelected) {
+        aboutSelected = true;
+        resumeSelected = false;
+        githubSelected = false;
+        testSelected = false;
+        writeNewActionText('learn more about me!')
+    }
 })
 
-// document.querySelector("#about").addEventListener('mouseenter', (e) => {
-//     // actionTyping.delete(5).go();
-//     // actionTyping.reset();
-//     // aboutTyping.go();
+document.querySelector("#resume").addEventListener('mouseenter', (e) => {
+    if(!resumeSelected) {
+        aboutSelected = false;
+        resumeSelected = true;
+        githubSelected = false;
+        testSelected = false;
+        writeNewActionText('resume pop-up')
+    }
+})
 
-//     new TypeIt(".action-text", {
-//         waitUntilVisible: true,
-//         lifeLike: true
-//     }).delete(19).type('new-action').go()
-//     console.log("mouseenter");
-// })
+document.querySelector("#github").addEventListener('mouseenter', (e) => {
+    if(!githubSelected) {
+        aboutSelected = false;
+        resumeSelected = false;
+        githubSelected = true;
+        testSelected = false;
+        writeNewActionText('check out my github at https://github.com/PatrickKan/')
+    }
+})
+
+document.querySelector("#test").addEventListener('mouseenter', (e) => {
+    if(!testSelected) {
+        aboutSelected = false;
+        resumeSelected = false;
+        githubSelected = false;
+        testSelected = true;
+        writeNewActionText('test selected')
+    }
+})
+
 
 // document.querySelector("#about").addEventListener('mouseleave', (e) => {
-//     // actionTyping.freeze();
-//     // aboutTyping.reset();
-//     // actionTyping.go();
-//     console.log("mouseout");
+//     // typed.toggle()
+//     // typed.start()
+//     // typed.stop()
 // })
+
+function writeNewActionText(text) {
+    writeNewTextForId('#action-text-id', text)
+}
+
+function writeNewTextForId(id, text) {
+    typed.destroy();
+
+    typed = new Typed(id, {
+        strings: [text],
+        typeSpeed: 50,
+        backSpeed: 30,
+    })
+}
